e9e30c2cdafe25809d7c6c3e2bf531c427be30a9
mezin <z2941@ya.ru>
2023-05-20 18:31:04 +0300

    feat: добавил blur-эффект при загрузке фото добавил проверки на наличие свойст из данных АПИ

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import type { FC } from 'react'
+import { useState } from 'react'
 
 import type { GeneralData } from '@/types/MKRF.type'
 
@@ -9,30 +10,41 @@ type Props = {
   category?: string
 }
 
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 const Item: FC<Props> = ({ item, category }) => {
+  const [isLoading, setLoading] = useState(true)
   return (
-    <Link href={`/${category}/${item.id}`}>
-      <div className="relative aspect-square w-full hover:shadow-lg">
+    <Link href={`/${category}/${item?.id}`}>
+      <div className="relative aspect-square w-full overflow-hidden rounded-lg bg-gray-200 hover:shadow-lg">
         <Image
           fill={true}
-          src={item.image.url || '/'}
-          className="h-52 w-full rounded-md object-cover"
-          alt={item.name || ''}
-          loading="lazy"
+          src={item?.image?.url || ''}
+          alt={item?.name || ''}
+          style={{ transform: 'translate3d(0, 0, 0)' }}
+          className={cn(
+            'group-hover:opacity-75 duration-700 ease-in-out rounded-lg object-cover brightness-90 transition group-hover:brightness-110',
+            isLoading
+              ? 'grayscale blur-2xl scale-110'
+              : 'grayscale-0 blur-0 scale-100'
+          )}
+          onLoadingComplete={() => setLoading(false)}
         />
       </div>
       <div className="pt-2">
         <div className="">
           <p className="font-bold text-gray-900">
-            {item.category.name}, {item.locale.name}
+            {item?.category?.name}, {item?.locale?.name}
           </p>
         </div>
         <div className="">
-          <p className="truncate text-base text-gray-500">{item.name}</p>
+          <p className="truncate text-base text-gray-500">{item?.name}</p>
         </div>
         <div className="flex justify-between">
           <div>
-            <p className="text-[16px]">{item.organization.name}</p>
+            <p className="text-[16px]">{item?.organization?.name}</p>
           </div>
         </div>
       </div>
